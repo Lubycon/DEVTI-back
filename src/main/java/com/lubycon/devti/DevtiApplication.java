608c6168a3bb592b3b793c6d669865575004541c
SHKim-airi <dev.sanghyuk@gmail.com>
2021-04-16 18:50:13 +0900

    init commit

---
+++ src/main/java/com/lubycon/devti/DevtiApplication.java
@@ -0,0 +1,13 @@
+package com.lubycon.devti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevtiApplication.class, args);
+  }
+
+}
