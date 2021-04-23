e99505d2f08a6f63c9280afb247d5c39a41de974
SHKim-airi <dev.sanghyuk@gmail.com>
2021-04-23 15:59:57 +0900

    @Repository어노테이션 JpaRepository상속받기 때문에 삭제

--- src/main/java/com/lubycon/devti/domain/question/dao/QuestionRepository.java
+++ src/main/java/com/lubycon/devti/domain/question/dao/QuestionRepository.java
@@ -2,9 +2,7 @@ package com.lubycon.devti.domain.question.dao;
 
 import com.lubycon.devti.domain.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 }
--- src/main/java/com/lubycon/devti/domain/user/dao/UserRepository.java
+++ src/main/java/com/lubycon/devti/domain/user/dao/UserRepository.java
@@ -2,9 +2,7 @@ package com.lubycon.devti.domain.user.dao;
 
 import com.lubycon.devti.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
