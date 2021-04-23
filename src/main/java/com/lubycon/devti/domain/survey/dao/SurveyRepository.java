bb0cabf1497a367c1e23b45620e7408a7372c6ff
SHKim-airi <dev.sanghyuk@gmail.com>
2021-04-23 15:56:21 +0900

    SurveyRepository @Repository어노테이션 삭제

--- src/main/java/com/lubycon/devti/domain/survey/dao/SurveyRepository.java
+++ src/main/java/com/lubycon/devti/domain/survey/dao/SurveyRepository.java
@@ -2,9 +2,7 @@ package com.lubycon.devti.domain.survey.dao;
 
 import com.lubycon.devti.domain.survey.entity.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
 
 }
