ad03038552fcdb5065af6185125977edac5f33c7
SHKim-airi <dev.sanghyuk@gmail.com>
2021-04-23 13:07:52 +0900

    User <-> Suvey 연관관계 설정

--- src/main/java/com/lubycon/devti/domain/survey/entity/Survey.java
+++ src/main/java/com/lubycon/devti/domain/survey/entity/Survey.java
@@ -1,11 +1,16 @@
 package com.lubycon.devti.domain.survey.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.lubycon.devti.domain.user.entity.User;
 import com.lubycon.devti.global.entity.BaseTimeEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,4 +36,9 @@ public class Survey extends BaseTimeEntity {
   @Column
   private String surveyType;
   
+  @JsonBackReference
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
 }
