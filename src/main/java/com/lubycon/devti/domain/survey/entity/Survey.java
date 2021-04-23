f303d2e0629d9b2e1c7b96b66826203b24560be5
SHKim-airi <dev.sanghyuk@gmail.com>
2021-04-23 13:12:45 +0900

    Survey EnumType 추가 및 적용

--- src/main/java/com/lubycon/devti/domain/survey/entity/Survey.java
+++ src/main/java/com/lubycon/devti/domain/survey/entity/Survey.java
@@ -2,9 +2,12 @@ package com.lubycon.devti.domain.survey.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.lubycon.devti.domain.user.entity.User;
+import com.lubycon.devti.global.code.SurveyType;
 import com.lubycon.devti.global.entity.BaseTimeEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -33,9 +36,10 @@ public class Survey extends BaseTimeEntity {
   @Column(length = 500)
   private String comment;
 
+  @Enumerated(EnumType.STRING)
   @Column
-  private String surveyType;
-  
+  private SurveyType surveyType;
+
   @JsonBackReference
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id")
