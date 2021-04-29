5747fcd98d3edd361535a847feaabc5db88e7d1d
SHKim-airi <dev.sanghyuk@gmail.com>
2021-04-29 21:05:04 +0900

    Disable entity rest resource export

--- src/main/java/com/lubycon/devti/domain/option/dao/OptionRepository.java
+++ src/main/java/com/lubycon/devti/domain/option/dao/OptionRepository.java
@@ -2,7 +2,9 @@ package com.lubycon.devti.domain.option.dao;
 
 import com.lubycon.devti.domain.option.entity.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface OptionRepository extends JpaRepository<Option, Long> {
 
 }
--- src/main/java/com/lubycon/devti/domain/question/dao/QuestionRepository.java
+++ src/main/java/com/lubycon/devti/domain/question/dao/QuestionRepository.java
@@ -2,7 +2,9 @@ package com.lubycon.devti.domain.question.dao;
 
 import com.lubycon.devti.domain.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 }
