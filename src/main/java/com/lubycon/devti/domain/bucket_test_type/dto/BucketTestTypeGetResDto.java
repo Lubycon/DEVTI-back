11adc3facb64a9d23afaa96c95fcb58dee15da5c
SHKim-airi <dev.sanghyuk@gmail.com>
2021-05-05 16:04:18 +0900

    Refactor: Jetbrain annotation dependency추가 및 @NotNull적용

--- src/main/java/com/lubycon/devti/domain/bucket_test_type/dto/BucketTestTypeGetResDto.java
+++ src/main/java/com/lubycon/devti/domain/bucket_test_type/dto/BucketTestTypeGetResDto.java
@@ -3,11 +3,14 @@ package com.lubycon.devti.domain.bucket_test_type.dto;
 import com.lubycon.devti.global.code.TestType;
 import lombok.Builder;
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 
 @Getter
 public class BucketTestTypeGetResDto {
 
+  @NotNull
   private TestType testType;
+  @NotNull
   private String phrases;
 
   @Builder
