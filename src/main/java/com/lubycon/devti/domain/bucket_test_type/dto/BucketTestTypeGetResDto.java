929e6a579dadd8581b6e7a337c8bb7d5539b9bac
SHKim-airi <dev.sanghyuk@gmail.com>
2021-05-05 12:20:25 +0900

    Refactor: BucketTestResDto 생성자에 @builder방식으로 변경, 제약조건 삭제

--- src/main/java/com/lubycon/devti/domain/bucket_test_type/dto/BucketTestTypeGetResDto.java
+++ src/main/java/com/lubycon/devti/domain/bucket_test_type/dto/BucketTestTypeGetResDto.java
@@ -1,21 +1,18 @@
 package com.lubycon.devti.domain.bucket_test_type.dto;
 
 import com.lubycon.devti.global.code.TestType;
-import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class BucketTestTypeGetResDto {
 
-  @NotNull
   private TestType testType;
-  @NotNull
   private String phrases;
 
+  @Builder
+  public BucketTestTypeGetResDto(TestType testType, String phrases) {
+    this.testType = testType;
+    this.phrases = phrases;
+  }
 }
