687720fc4ef4983639284e5cc71d2bd27efef998
SHKim-airi <dev.sanghyuk@gmail.com>
2021-05-05 15:46:22 +0900

    Feat: 사전참여 신청 API test_type추가 및 중복 검사 로직 추가

--- src/main/java/com/lubycon/devti/domain/survey/service/SurveyService.java
+++ src/main/java/com/lubycon/devti/domain/survey/service/SurveyService.java
@@ -4,6 +4,9 @@ import com.lubycon.devti.domain.survey.dao.SurveyRepository;
 import com.lubycon.devti.domain.survey.dto.SurveyPostDto.SurveyPostReqDto;
 import com.lubycon.devti.domain.survey.dto.SurveyPostDto.SurveyPostResDto;
 import com.lubycon.devti.domain.survey.entity.Survey;
+import com.lubycon.devti.global.error.ErrorCode;
+import com.lubycon.devti.global.error.exception.InvalidValueException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,19 +21,37 @@ public class SurveyService {
 
   @Transactional
   public SurveyPostResDto createSurvey(SurveyPostReqDto surveyPostReqDto) {
+    Optional<Survey> checkSurvey = null;
+
+    if (null != surveyPostReqDto.getPhone()) {
+      checkSurvey = surveyRepository.findByPhone(surveyPostReqDto.getPhone());
+      if (checkSurvey.isPresent()) {
+        throw new InvalidValueException(surveyPostReqDto.getEmail(), ErrorCode.PHONE_DUPLICATION);
+      }
+    } else {
+      checkSurvey = surveyRepository.findByEmail(surveyPostReqDto.getEmail());
+      if (checkSurvey.isPresent()) {
+        throw new InvalidValueException(surveyPostReqDto.getEmail(), ErrorCode.EMAIL_DUPLICATION);
+      }
+    }
 
     Survey survey = Survey.builder()
         .comment(surveyPostReqDto.getComment())
         .surveyType(surveyPostReqDto.getSurveyType())
         .email(surveyPostReqDto.getEmail())
+        .phone(surveyPostReqDto.getPhone())
+        .testType(surveyPostReqDto.getTestType())
         .build();
+
     Survey savedSurvey = surveyRepository.save(survey);
 
     return SurveyPostResDto.builder()
         .id(savedSurvey.getId())
         .comment(savedSurvey.getComment())
         .email(savedSurvey.getEmail())
+        .phone(savedSurvey.getPhone())
+        .testType(savedSurvey.getTestType())
         .build();
-  }
 
+  }
 }
