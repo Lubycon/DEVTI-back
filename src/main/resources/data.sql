a8ae36f4b44ad9f8ca750b08634931c6f7449710
SHKim-airi <dev.sanghyuk@gmail.com>
2021-05-07 22:22:10 +0900

    Feat: TestType추가 및 survey data 삽입 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,5 @@
-INSERT INTO survey (created_at, updated_at, comment, email, survey_type, test_type) VALUES (now(), now(), 'FE,BE? ', 'user1@devti.com', 'DEVTI', 'TYPE_COMMON_1');
-INSERT INTO survey (created_at, updated_at, comment, email, survey_type, test_type) VALUES (now(), now(), 'FE,BE? ', 'user2@devti.com', 'DEVTI', 'TYPE_COMMON_2');
-INSERT INTO survey (created_at, updated_at, comment, phone, survey_type, test_type) VALUES (now(), now(), 'FE,BE? ', '010-1111-2222', 'DEVTI', 'TYPE_COMMON_3');
-INSERT INTO survey (created_at, updated_at, comment, phone, survey_type, test_type) VALUES (now(), now(), 'FE,BE? ', '010-3333-4444', 'DEVTI','TYPE_MOM_CAFE_1');
-
-INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "완전 현직 개발자 type", "나에게 딱 맞는 개발자 직군을 찾아보세요", 'TYPE_COMMON_1');
-INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "개발자 준비 중 취업러 type", "지금, 어떤 직군에 지원해야 하는지 고민이 된다면?", 'TYPE_COMMON_2');
-INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "성향 테스트를 좋아하는 사람 type", "당신도 신입 6000연봉 가능합니다", 'TYPE_COMMON_3');
-INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "아이들은 위한 엄마들의 맘카페 type", "우리 아이의 코딩 잠재력을 바로 확인하세요", 'TYPE_MOM_CAFE_1');
+INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "완전 현직 개발자 type", "나에게 딱 맞는\n개발자 직군을 찾아보세요", 'TYPE_COMMON_1');
+INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "개발자 준비 중 취업러 type", "지금 백엔드와 프론트엔드\n갈림길에 있나요?", 'TYPE_COMMON_2');
+INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "기업 성향을 알아보고 싶은 type", "나는 스타트업 성향일까\n대기업 성향일까?", 'TYPE_COMMON_3');
+INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "성향 테스트를 좋아하는 사람 type", "나도 개발자가 된다면?\n코딩지식없이 테스트하기!", 'TYPE_COMMON_4');
+INSERT INTO bucket_test_type(created_at, updated_at, description, phrases, test_type) VALUES(now(), now(), "아이들은 위한 엄마들의 맘카페 type", "우리 아이의 코딩 잠재력을\n바로 확인하세요!", 'TYPE_MOM_CAFE_1');
