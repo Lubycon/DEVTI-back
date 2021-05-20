14d20c38c8894c251f55d340433a9e2dd8712c9a
SHKim-airi <dev.sanghyuk@gmail.com>
2021-05-20 21:02:31 +0900

    Feat: AnswerType enum 추가

---
+++ src/main/java/com/lubycon/devti/global/code/AnswerType.java
@@ -0,0 +1,23 @@
+package com.lubycon.devti.global.code;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum AnswerType implements DevtiEnumerable {
+  PRESET(0),
+  GAGE(1),
+  ;
+
+  private final int value;
+
+  @Override
+  public int getValue() {
+    return value;
+  }
+
+  @Override
+  public String getKey() {
+    return name();
+  }
+
+}
