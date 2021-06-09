902a1c153631c5e70a9386953a66dcbbeb07aa41
SHKim-airi <dev.sanghyuk@gmail.com>
2021-06-09 21:05:43 +0900

    Refactor: Pillar enum 생성자 수정

--- src/main/java/com/lubycon/devti/global/code/Pillar.java
+++ src/main/java/com/lubycon/devti/global/code/Pillar.java
@@ -2,9 +2,7 @@ package com.lubycon.devti.global.code;
 
 import java.util.Arrays;
 import java.util.List;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 public enum Pillar {
   ROLE("직군", Arrays.asList("F", "B")),
   SCALE("규모", Arrays.asList("S", "C")),
@@ -12,7 +10,13 @@ public enum Pillar {
   PRIORITY("우선순위", Arrays.asList("W", "L")),
   ;
 
+  private final String pillar;
+  public final List<String> biasList;
 
   Pillar(String pillar, List<String> biasList) {
+    this.pillar = pillar;
+    this.biasList = biasList;
   }
+  
+
 }
